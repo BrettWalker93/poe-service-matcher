e8185eca1761ece017f6828adf14fe2d451700eb
Brett Walker <brettwalker1337@gmail.com>
2023-05-25 11:52:06 -0500

    $service command

--- poe_service_matcher/commands/bot_commands.py
+++ poe_service_matcher/commands/bot_commands.py
@@ -1,6 +1,8 @@
 import os
 import discord
 from discord.ext import commands
+from ..models import ServiceListing
+from ...poe_service_matcher import db, app
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,9 +18,22 @@ async def on_message(message):
     if message.content.startswith('$service'):
 
         def parse(m):
-            return m.author == message.author
+            valid = False
+
+            mm = m.content.split(',')
+
+            if len(mm) == 4 and m.author == message.author:
+                valid = True
+
+                new_service = ServiceListing(user_id=m.author.toString(), service=mm[0], map_provided=mm[1], slots=mm[2], price=mm[3])
+                with app.app_context():
+                    db.session.add(new_service)
+                    db.session.commit()
+
+            return valid
         
         try:
+            message.channel.send('service name, map provided (y/n), slots, price in chaos \n example: \'uber sirus, y, 1, 50\'' )
             response = await bot.wait_for('message', check=parse, timeout=300)
             await message.channel.send(f'{response.content}')
         except asyncio.TimeoutError:

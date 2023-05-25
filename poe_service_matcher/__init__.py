046e47bbdc0f1d650981dd2ebbbbbc68e66b3ff1
Brett Walker <brettwalker1337@gmail.com>
2023-05-25 09:44:50 -0500

    root index

--- poe_service_matcher/__init__.py
+++ poe_service_matcher/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    services = ServiceListing.query.all()
+    return render_template('index.html', services=services)
 
 from .models import User, ServiceListing

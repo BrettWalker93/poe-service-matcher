ec37aab6ddc10fcb85c23f1d1841ad72ddbbe675
Brett Walker <brettwalker1337@gmail.com>
2023-05-25 13:32:42 -0500

    wrap

--- poe_service_matcher/commands/bot_commands.py
+++ poe_service_matcher/commands/bot_commands.py
@@ -67,6 +67,9 @@ async def on_message(message):
 
     elif message.content.startswith('$request'):
 
+        def parse_wrapper(m):
+            return asyncio.ensure_future(parse(m))
+
         async def parse(m):
 
             #print(f'parsing: {m.content}')
@@ -102,7 +105,7 @@ async def on_message(message):
         try:
             await message.channel.send('service name?')
             #print("Before bot.wait_for")
-            response = await bot.wait_for('message', check=await parse, timeout=300)
+            response = await bot.wait_for('message', check=parse_wrapper timeout=300)
             #print("After bot.wait_for")
             await message.channel.send(f'{response.content}')
         except asyncio.TimeoutError:

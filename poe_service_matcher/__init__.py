fc6d5b34381a29da2683f66ccfa382e54de53535
Brett Walker <brettwalker1337@gmail.com>
2023-05-25 11:18:48 -0500

    ../

--- poe_service_matcher/__init__.py
+++ poe_service_matcher/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__, template_folder = 'templates')
+app = Flask(__name__, template_folder = '../templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 from .database import db

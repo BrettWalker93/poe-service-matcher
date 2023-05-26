0e1ff8b61895b1527b37d16bf093bb0ba32c079e
Brett Walker <brettwalker1337@gmail.com>
2023-05-26 09:55:02 -0500

    app context

--- poe_service_matcher/commands/bot_commands.py
+++ poe_service_matcher/commands/bot_commands.py
@@ -21,70 +21,70 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    with app.app_context():
+        if message.author == bot.user:
+            return
 
-    if message.author == bot.user:
-        return
+        print(f'received message: {message.content} from {message.author}')
 
-    print(f'received message: {message.content} from {message.author}')
+        user = None
+        with scoped_session(sessionmaker(bind=db.engine)) as session:
+            user = dbs.get_user(str(message.author.id), session)
 
-    user = None
-    with scoped_session(sessionmaker(bind=db.engine)) as session:
-        user = dbs.get_user(str(message.author.id), session)
+            #print(f'{user.username}')
 
-        #print(f'{user.username}')
+        if message.content.startswith('$service'):
 
-    if message.content.startswith('$service'):
+            def parse(m):
+                valid = False
 
-        def parse(m):
-            valid = False
+                mm = [item.strip() for item in m.content.split(',')]
 
-            mm = [item.strip() for item in m.content.split(',')]
-
-            if len(mm) == 4 and m.author == message.author:
-                valid = True
-                with scoped_session(sessionmaker(bind=db.engine)) as session:
-                    dbs.list_service(mm, user, session)
-
-            return valid
-        
-        try:
-            await message.channel.send('service name, map provided (y/n), slots, price in chaos \n example: \'uber sirus, y, 1, 50\'' )
-            response = await bot.wait_for('message', check=parse, timeout=300)
-            await message.channel.send(f'{response.content}')
-        except asyncio.TimeoutError:
-            await message.channel.send('poop de pewp de pantzes')
-
-    elif message.content.startswith('$request'):
-
-        def parse_wrapper(m):
-            return asyncio.ensure_future(parse(m))
-
-        async def parse(m):
-
-            #print(f'parsing: {m.content}')
-            with scoped_session(sessionmaker(bind=db.engine)) as session:
-                valid = dbs.service_exists(m.content, session)
-
-                if valid:
-                    response_message = dbs.parse_request(m.content, session)
-                    await message.channel.send(m.content)
-                    await message.channel.send(response_message)
-                else:
-                    await message.channel.send('Service not currently listed.')
+                if len(mm) == 4 and m.author == message.author:
+                    valid = True
+                    with scoped_session(sessionmaker(bind=db.engine)) as session:
+                        dbs.list_service(mm, user, session)
 
                 return valid
+        
+            try:
+                await message.channel.send('service name, map provided (y/n), slots, price in chaos \n example: \'uber sirus, y, 1, 50\'' )
+                response = await bot.wait_for('message', check=parse, timeout=300)
+                await message.channel.send(f'{response.content}')
+            except asyncio.TimeoutError:
+                await message.channel.send('poop de pewp de pantzes')
 
-        try:
-            await message.channel.send('service name?')
-            #print("Before bot.wait_for")
-            response = await bot.wait_for('message', check=parse_wrapper, timeout=300)
-            #print("After bot.wait_for")
-            #await message.channel.send(f'{response.content}')
-        except asyncio.TimeoutError:
-            await message.channel.send('poop de pewp de pantzes?')
+        elif message.content.startswith('$request'):
 
-    elif message.content.startswith('$clear'):
+            def parse_wrapper(m):
+                return asyncio.ensure_future(parse(m))
 
-        if user.username == '168865934675542016':
-            with scoped_session(sessionmaker(bind=db.engine)) as session:
-                dbs.clear_listings(session)
+            async def parse(m):
+
+                #print(f'parsing: {m.content}')
+                with scoped_session(sessionmaker(bind=db.engine)) as session:
+                    valid = dbs.service_exists(m.content, session)
+
+                    if valid:
+                        response_message = dbs.parse_request(m.content, session)
+                        await message.channel.send(m.content)
+                        await message.channel.send(response_message)
+                    else:
+                        await message.channel.send('Service not currently listed.')
+
+                    return valid
+
+            try:
+                await message.channel.send('service name?')
+                #print("Before bot.wait_for")
+                response = await bot.wait_for('message', check=parse_wrapper, timeout=300)
+                #print("After bot.wait_for")
+                #await message.channel.send(f'{response.content}')
+            except asyncio.TimeoutError:
+                await message.channel.send('poop de pewp de pantzes?')
+
+        elif message.content.startswith('$clear'):
+
+            if user.username == '168865934675542016':
+                with scoped_session(sessionmaker(bind=db.engine)) as session:
+                    dbs.clear_listings(session)
